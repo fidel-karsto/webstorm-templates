3e5110dbfafb1d56c92cee6698f5931ba2ec8654
Karsten Rieger <fidel.karsto@googlemail.com>
2014-04-16 00:29:09 +0200

    added xml variables for processing

--- tasks/postprocess.js
+++ tasks/postprocess.js
@@ -5,30 +5,25 @@ var fs = require('fs'),
 
 module.exports = function (grunt) {
     'use strict';
-    function wrapAndReplace() {
-        var placeHolderStr = '####@@####',
-            placeHolderRegExp = new RegExp(placeHolderStr, 'g');
 
-        return function (str) {
-            //TODO build closure
-        };
-    }
+    var PLACEHOLDER_WITH_DEFAULT_G = /\$\{(\d+):(\w+)\}/g,
+        PLACEHOLDER_WITH_DEFAULT   = /\$\{(\d+):(\w+)\}/,
+        PLACEHOLDER_NO_DEFAULT_G   = /\$\{(\d+):\}/g,
+        PLACEHOLDER_NO_DEFAULT     = /\$\{(\d+):\}/,
+        PLACEHOLDER_END_G          = /\$0/g,
+        PLACEHOLDER_END            = /\$0/,
+        FIELD_POINTS_G             = /\$(\d+)/g,
+        FIELD_POINTS               = /\$(\d+)/;
 
     grunt.registerTask('postProcessSnippets', 'Text based post processing of transformed sublime-snippets', function() {
         var done = this.async(),
             files = grunt.file.expand(grunt.config('postProcessSnippets.options.files')),
-            placeHolderEND = /\$0/g,
-            placeholderWithDefault = /\$\{(\d+):(\w+)\}/g,
-            placeholderNoDefault = /\$\{(\d+):\}/g,
-            fieldPoints = /\$(\d+)/g,
-            placeHolderStr = '####@@####',
-            placeHolderRegExp = new RegExp(placeHolderStr, 'g'),
-            domParser = new xmldom.DOMParser(),
-            xmlSerializer = new xmldom.XMLSerializer();
+            placeHolderStr  = '####@@####',
+            placeHolderRegEx = new RegExp(placeHolderStr, 'g'),
+            domParser       = new xmldom.DOMParser(),
+            xmlSerializer   = new xmldom.XMLSerializer();
 
         console.log('running postProcess', files);
-
-        // <variable name="DEPENDENCIES" expression="" defaultValue="deps" alwaysStopAt="true" />
 
         files.forEach(function(file) {
             var template = grunt.file.read(file),
@@ -41,35 +36,58 @@ module.exports = function (grunt) {
                 tmplEl;
             console.log('processing ', template);
 
-            if (placeholderNoDefault.test(template)) {
+            doc = domParser.parseFromString(template, 'text/xml');
+            tmplEl = [].slice.call(doc.getElementsByTagName('template'));
 
-                doc = domParser.parseFromString(template, 'text/xml');
-                tmplEl = [].slice.call(doc.getElementsByTagName('template'));
+            if (tmplEl) {
+                tmplEl.forEach(function(tmpl) {
+                    tmplStr = tmpl.getAttribute('value');
 
-                if (tmplEl) {
-                    tmplEl.forEach(function(tmpl) {
-                        tmplStr = tmpl.getAttribute('value');
+                    while(defaults = PLACEHOLDER_WITH_DEFAULT_G.exec(tmplStr)){
+                        customVars.push({
+                            index: defaults[1],
+                            defaultValue: defaults[2],
+                            name: 'MYVAR' + defaults[1]
+                        });
+                        //console.log('defaults', customVars);
+                        tmplStr = tmplStr.replace(PLACEHOLDER_WITH_DEFAULT, placeHolderStr + defaults[1] + placeHolderStr).replace(placeHolderRegEx, '$');
+                    }
 
-                        defaults = placeholderWithDefault.exec(tmplStr);
-                        noDefaults = placeholderNoDefault.exec(tmplStr);
-                        fieldPts = fieldPoints.exec(tmplStr);
+                    while(noDefaults = PLACEHOLDER_NO_DEFAULT_G.exec(tmplStr)){
+                        customVars.push({
+                            index: noDefaults[1],
+                            defaultValue: null,
+                            name: 'MYVAR' + noDefaults[1]
+                        });
+                        //console.log('no defaults', noDefaults);
+                        tmplStr = tmplStr.replace(PLACEHOLDER_NO_DEFAULT, '$CUSTOMVAR$');
+                    }
 
-                        console.log('defaults', defaults);
-                        console.log('no defaults', noDefaults);
-                        console.log('field points', fieldPts);
+                    while(fieldPts = FIELD_POINTS_G.exec(tmplStr)){
+                        customVars.push({
+                            index: fieldPts[1],
+                            defaultValue: null,
+                            name: 'MYVAR' + fieldPts[1]
+                        });
+                        //console.log('field points', fieldPts);
+                        //tmplStr = tmplStr.replace(FIELD_POINTS, '$_$');
+                    }
 
-                        tmplStr = tmplStr.replace(placeHolderEND, '$END$');
-                        tmplStr = tmplStr.replace(placeholderWithDefault, placeHolderStr+'$2'+placeHolderStr).replace(placeHolderRegExp, '$');
-                        tmplStr = tmplStr.replace(placeholderNoDefault, '$CUSTOMVAR$');
+                    tmplStr = tmplStr.replace(PLACEHOLDER_END_G, '$END$');
+                    tmpl.setAttribute('value', tmplStr);
 
-                        tmplEl[0].setAttribute('value', tmplStr);
-
-                        console.log('value: ', tmpl.getAttribute('value'));
+                    customVars.forEach(function(customVar){
+                        var variableEl = doc.createElement('variable');
+                        variableEl.setAttribute('name', customVar.name);
+                        variableEl.setAttribute('expression', '');
+                        variableEl.setAttribute('defaultValue', customVar.defaultValue || '');
+                        variableEl.setAttribute('alwaysStopAt', 'true');
+                        tmpl.appendChild(variableEl);
                     });
-                }
-
-                console.log(xmlSerializer.serializeToString(doc));
+                });
             }
+
+            console.log(xmlSerializer.serializeToString(doc));
         });
 
         done();
